64b0f89b020a10d9585cc1cd54558a0dea8326f8
notRichtiger <notrichtiger@proton.me>
2026-06-21 00:18:56 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'spark-mod-watch-v1';
+const CACHE = 'spark-mod-watch-v2';
 const SHELL = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e=>{
